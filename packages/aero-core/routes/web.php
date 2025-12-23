925be1ddfcf8da8b78a05326d5b2aa394131851d
Emam Hosen <emamhsajeeb@gmail.com>
2025-12-23 14:06:12 +0600

    ok

--- packages/aero-core/routes/web.php
+++ packages/aero-core/routes/web.php
@@ -11,6 +11,7 @@ use Aero\Core\Http\Controllers\Auth\EmailVerificationController;
 use Aero\Core\Http\Controllers\Auth\NewPasswordController;
 use Aero\Core\Http\Controllers\Auth\PasswordResetLinkController;
 use Aero\Core\Http\Controllers\DashboardController;
+use Aero\Core\Http\Controllers\InstallationController;
 use Aero\Core\Http\Controllers\Settings\SystemSettingController;
 use Aero\Core\Services\PlatformErrorReporter;
 use Illuminate\Http\Request;
@@ -18,6 +19,29 @@ use Illuminate\Support\Facades\Route;
 
 // Note: TenantOnboardingController is referenced dynamically if platform package is installed
 // We don't use a 'use' statement here since it may not exist
+
+/*
+|--------------------------------------------------------------------------
+| Installation Routes (Standalone Mode Only)
+|--------------------------------------------------------------------------
+*/
+if (config('aero.mode') === 'standalone') {
+    Route::middleware('web')->prefix('install')->name('install.')->group(function () {
+        Route::get('/', [InstallationController::class, 'index'])->name('index');
+        Route::get('/license', [InstallationController::class, 'license'])->name('license');
+        Route::post('/validate-license', [InstallationController::class, 'validateLicense'])->name('validate-license');
+        Route::get('/requirements', [InstallationController::class, 'requirements'])->name('requirements');
+        Route::get('/database', [InstallationController::class, 'database'])->name('database');
+        Route::post('/test-database', [InstallationController::class, 'testDatabase'])->name('test-database');
+        Route::get('/application', [InstallationController::class, 'application'])->name('application');
+        Route::post('/save-application', [InstallationController::class, 'saveApplication'])->name('save-application');
+        Route::post('/test-email', [InstallationController::class, 'testEmail'])->name('test-email');
+        Route::get('/admin', [InstallationController::class, 'admin'])->name('admin');
+        Route::post('/save-admin', [InstallationController::class, 'saveAdmin'])->name('save-admin');
+        Route::post('/install', [InstallationController::class, 'install'])->name('process');
+        Route::get('/progress', [InstallationController::class, 'progress'])->name('progress');
+    });
+}
 
 /*
 |--------------------------------------------------------------------------
